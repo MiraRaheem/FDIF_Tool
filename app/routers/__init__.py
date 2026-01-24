16ed06a5741e3686790e70bf7d809ab40a01df78
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-01-24 15:03:08 +0200

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,5 @@
 from .ingest import router as ingest_router
+from .harmonize import router as harmonize_router
 from .validate import router as validate_router
 from .iot import router as iot_router
-from .harmonize import router as harmonize_router
+from .blueprint import router as blueprint_router
