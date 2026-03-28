1694d3de7497561cef442721d7184ca8367d3388
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-03-28 17:38:55 +0200

    Add medwood and budatec routers to init.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,2 @@
-from .ingest import router as ingest_router
-from .harmonize import router as harmonize_router
-from .validate import router as validate_router
-from .iot import router as iot_router
-from .blueprint import router as blueprint_router
-from .work_order import router as work_order_router
+from .medwood import router as medwood
+from .budatec import router as budatec
