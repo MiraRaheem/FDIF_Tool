6e90e82628571022d5dd4248e444e92a9f953d65
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-03-28 18:08:24 +0200

    Update harmonizer_budatec.py

--- app/services/harmonizer_budatec.py
+++ app/services/harmonizer_budatec.py
@@ -1,3 +1,6 @@
+def to_bool(v):
+    return str(v) in ["1", "true", "True"]
+    
 def harmonize_budatec_supplier(raw):
 
     return {
@@ -11,22 +14,22 @@ def harmonize_budatec_supplier(raw):
         "language": raw.get("language"),
         "primaryContact": raw.get("supplier_primary_contact"),
 
-        "isTransportProvider": bool(raw.get("is_transporter")),
-        "isInternalSupplier": bool(raw.get("is_internal_supplier")),
+        "isTransportProvider": to_bool(raw.get("is_transporter")),
+        "isInternalSupplier": to_bool(raw.get("is_internal_supplier")),
         "representsCompany": raw.get("represents_company"),
 
-        "isFrozen": bool(raw.get("is_frozen")),
-        "isDisabled": bool(raw.get("disabled")),
-        "isOnHold": bool(raw.get("on_hold")),
+        "isFrozen": to_bool(raw.get("is_frozen")),
+        "isDisabled": to_bool(raw.get("disabled")),
+        "isOnHold": to_bool(raw.get("on_hold")),
         "holdType": raw.get("hold_type"),
 
         "operationalPolicy": {
-            "allowInvoiceWithoutPO": raw.get("allow_purchase_invoice_creation_without_purchase_order"),
-            "allowInvoiceWithoutReceipt": raw.get("allow_purchase_invoice_creation_without_purchase_receipt"),
-            "warnRFQ": raw.get("warn_rfqs"),
-            "warnPO": raw.get("warn_pos"),
-            "preventRFQ": raw.get("prevent_rfqs"),
-            "preventPO": raw.get("prevent_pos"),
+            "allowInvoiceWithoutPO": to_bool(raw.get("allow_purchase_invoice_creation_without_purchase_order")),
+            "allowInvoiceWithoutReceipt": to_bool(raw.get("allow_purchase_invoice_creation_without_purchase_receipt")),
+            "warnRFQ": to_bool(raw.get("warn_rfqs")),
+            "warnPO": to_bool(raw.get("warn_pos")),
+            "preventRFQ": to_bool(raw.get("prevent_rfqs")),
+            "preventPO": to_bool(raw.get("prevent_pos")),
         },
 
         "metadata": {
