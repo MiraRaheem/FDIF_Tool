b916b5ef2a8b99b54f17dc8f7895b001a6cc9ceb
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-04-17 00:50:10 +0200

    Create validator_frank_event.py

---
+++ app/services/validator_frank_event.py
@@ -0,0 +1,29 @@
+def validate_frank_event(data):
+
+    # -------- REQUIRED --------
+    if not data.get("eventType"):
+        raise ValueError("eventType missing")
+
+    if not data.get("timestamp"):
+        raise ValueError("timestamp missing")
+
+    if data.get("machineId") is None:
+        raise ValueError("machineId missing")
+
+    # -------- NUMERIC --------
+    numeric_fields = [
+        "storageTemperature",
+        "storageHumidity",
+        "averagePowerConsumption",
+        "compressedAirInput",
+        "noiseChillerLevel",
+        "operatingTemperature",
+        "powerPeakConsumption",
+        "nozzleOperationTime"
+    ]
+
+    for field in numeric_fields:
+        if data.get(field) is not None:
+            data[field] = float(data[field])
+
+    return data
