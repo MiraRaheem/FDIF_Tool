73c295c65a82ca9dca13b0f8eb1640d6a8852462
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-04-17 00:49:45 +0200

    Add harmonize_frank_event function to process events

---
+++ app/services/harmonizer_frank_event.py
@@ -0,0 +1,25 @@
+def harmonize_frank_event(raw):
+
+    return {
+
+        # -------- CORE --------
+        "eventType": raw.get("eventType"),
+        "timestamp": raw.get("time"),
+        "timestampHR": raw.get("timeHR"),
+
+        # -------- MACHINE --------
+        "machineId": raw.get("machineId"),
+
+        # -------- SENSOR DATA --------
+        "storageTemperature": raw.get("storageTemperature"),
+        "storageHumidity": raw.get("storageHumidity"),
+        "averagePowerConsumption": raw.get("averagePowerConsumption"),
+        "compressedAirInput": raw.get("compressedAirInput"),
+        "noiseChillerLevel": raw.get("noiseChillerLevel"),
+        "operatingTemperature": raw.get("operatingTemperature"),
+        "powerPeakConsumption": raw.get("powerPeakConsumption"),
+
+        # -------- STATUS --------
+        "machineError": raw.get("machineError"),
+        "nozzleOperationTime": raw.get("nozzleOperationTime"),
+    }
