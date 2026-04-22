a88bec68c6908cc3ec4d0b1f1567cccfc342d80f
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-04-23 00:15:41 +0200

    Add validation for argon prediction data

--- app/services/validator_events.py
+++ app/services/validator_events.py
@@ -18,3 +18,24 @@ def validate_events(data):
             raise HTTPException(
                 400, f"Parameter {r['type']} does not exist"
             )
+
+from fastapi import HTTPException
+from app.services.blueprint_adapter import instance_exists
+
+
+def validate_argon_prediction(data):
+
+    machine_id = f"Machine_{data['machineId']}"
+    sensor_id = data["sensorId"]
+
+    if not instance_exists("Machine", machine_id):
+        raise HTTPException(400, f"Machine {machine_id} does not exist")
+
+    if not instance_exists("ConditionMonitoringSensor", sensor_id):
+        raise HTTPException(400, f"Sensor {sensor_id} does not exist")
+
+    for r in data["basedOn"]:
+        if not instance_exists("PhysicalParameter", r["type"]):
+            raise HTTPException(
+                400, f"Parameter {r['type']} does not exist"
+            )
