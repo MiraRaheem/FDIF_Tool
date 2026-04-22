4e883b3dbabacf9f84600654fe177e4d02a7d700
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-04-23 00:15:04 +0200

    Add harmonize_argon_prediction function

--- app/services/harmonizer_events.py
+++ app/services/harmonizer_events.py
@@ -11,3 +11,23 @@ def harmonize_events(raw):
         "description": f"{raw['error']['code']} - {raw['error']['description']}",
         "triggerReadings": raw["triggerReadings"]
     }
+
+def harmonize_argon_prediction(raw):
+
+    prediction = raw["prediction"]
+
+    description = (
+        f"Predicted {prediction['target']} in "
+        f"{prediction['predictedTimeToDepletion']} {prediction['unit']} "
+        f"using {prediction['method']}"
+    )
+
+    return {
+        "eventId": raw["eventId"],
+        "machineId": raw["machineId"],
+        "timestamp": raw["timestamp"],
+        "sensorId": raw["virtualSensorId"],
+        "severity": raw["severity"],
+        "description": description,
+        "basedOn": raw["basedOn"]
+    }
