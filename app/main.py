79d583253d98f5693f801ade87f8c357d3b46bc2
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-04-23 21:33:13 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ from app.routers.medwood import router as medwood_router
 from app.routers.cep_router import router as cep_router  # ✅ NEW
 from app.routers.ontology_bootstrap import router as ontology_router
 from app.routers.cep_data_router import router as cep_data_router
+from app.routers.budatec.item import router as item_router
+
+
 
 app = FastAPI(
     title="FDIF PoC",
@@ -18,7 +21,7 @@ app = FastAPI(
 # Budatec
 app.include_router(supplier_router)
 app.include_router(customer_router)
-
+app.include_router(item_router)
 
 # Medwood
 app.include_router(medwood_router, prefix="/fdif")
