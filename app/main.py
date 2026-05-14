aa889c5b6e3cc24453c4552947b75870da0db9e0
Mira Raheem <43935614+MiraRaheem@users.noreply.github.com>
2026-05-15 02:53:37 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.routers.cep_data_router import router as cep_data_router
 from app.routers.budatec.item import router as item_router
 from app.routers.medwood.station import router as medwood_station_router
 from app.routers.medwood.material import router as medwood_material_router
+from app.routers.medwood.component import router as medwood_component_router
 
 
 
@@ -29,6 +30,7 @@ app.include_router(item_router)
 app.include_router(medwood_supplier_router)
 app.include_router(medwood_station_router)
 app.include_router(medwood_material_router)
+app.include_router(medwood_component_router)
 # CEP (Melito + future real-time ingestion)
 app.include_router(cep_router)  # ✅ NEW
 app.include_router(cep_data_router)
